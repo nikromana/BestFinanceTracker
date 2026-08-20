3ac46a6873d44c93a80c2f71a1721675d82f7029
Roman Nikitin <nikromyt@gmail.com>
2026-08-20 12:13:55 +0200

    repository and checn it works

--- BestFinanceTracker.Api/Program.cs
+++ BestFinanceTracker.Api/Program.cs
@@ -1,3 +1,9 @@
+using BestFinanceTracker.Application.Interfaces;
+using BestFinanceTracker.Application.Services;
+using BestFinanceTracker.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using BestFinanceTracker.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,9 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(connectionString!));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
---
+++ BestFinanceTracker.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using BestFinanceTracker.Application.Interfaces;
+using BestFinanceTracker.Domain.Entities;
+using BestFinanceTracker.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestFinanceTracker.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetAllAsync()
+        {
+            return await _context.Categories.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Category category)
+        {
+            _context.Categories.Remove(category);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
