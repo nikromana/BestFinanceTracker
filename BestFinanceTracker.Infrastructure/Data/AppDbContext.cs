60b902e22c24bd9034b198b06c73cc561efb9869
Roman Nikitin <nikromyt@gmail.com>
2026-08-20 11:48:37 +0200

    docker file, enitity frameworks packages were added

---
+++ BestFinanceTracker.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BestFinanceTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestFinanceTracker.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Entities.Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Name).IsRequired().HasMaxLength(100);
+                entity.Property(c => c.TransactionType).IsRequired();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
